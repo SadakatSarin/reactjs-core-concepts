3708230a50be6c711ed2467f8192b78ec5f98dc4
unknown <SadakatSarin>
2024-01-17 10:10:19 +0600

    1st

--- src/App.js
+++ src/App.js
@@ -1,22 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
 
+
+const number = 5555;
+
+const singer = {
+  
+  name: 'Dr Mahfuz',
+  job:'Singer'
+}
+
+const singerStyle = {
+  
+  color: 'purple'
+}
+
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
+        <h1 style={{backgroundColor:'red'}}>JSX</h1>
+        <h2 >Sarin</h2>
+        <div className="container">
+          
+
+          <h3>How are you?</h3>
+          
+
+        </div>
+
+        <div className="music">
+          <p style={singerStyle}>Name:{number + 2000}</p>
+          <p style={singerStyle}>Singer:{singer.name} {singer.job }</p>
+        </div>
+
+       
       </header>
     </div>
   );
