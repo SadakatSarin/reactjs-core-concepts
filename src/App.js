77b6f32f2a3b37979943e45924ca5e74c2a65da9
unknown <SadakatSarin>
2024-01-17 10:33:11 +0600

    commit3

--- src/App.js
+++ src/App.js
@@ -5,11 +5,49 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <Person></Person>
+      <h5>New component</h5>
+      <p>Rock mama react mama</p>
+      <Friend></Friend>
+
     </div>
   );
 }
 
+
+function Person(){
+
+  return (
+    //component
+    <div className='person'> 
+      <h1>Hello</h1>
+      <p>sarin</p>
+    
+  </div>
+  
+  )
+
+}
+
+
+function Friend() {
+  
+
+  return (
+
+    <div className='person'> 
+
+      <h3>Ajay Devgan</h3>
+      <p>Job:Maramari</p>
+
+    </div>
+
+
+  )
+}
 
 
 
